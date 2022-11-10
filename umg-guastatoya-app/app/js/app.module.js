3433976432739542546c41de6b12eb9fc3e630ee
jaldanaa <lioaldana6@gmail.com>
2022-11-10 17:26:58 -0600

    evaluaciones

--- umg-guastatoya-app/app/js/app.module.js
+++ umg-guastatoya-app/app/js/app.module.js
@@ -1,2 +1,2 @@
 angular.module('UniversidadApp', ['ui.router', 'ngAnimate', 'ngSanitize', 'ui.bootstrap', 'blockUI'])
-    .constant('apiUrl', 'http://18.213.116.160:80/api/');
+    .constant('apiUrl', 'http://127.0.0.1:8000/api/');
